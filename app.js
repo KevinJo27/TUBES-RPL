13b8348471202f279742faba4e87c70656e9420c
Devlin <devlin.venedict@gmail.com>
2023-11-30 21:27:04 +0700

    login dan signup berhasil di linked (js)

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+/*import express from "express";
+import mysql from "mysql";
+import bodyParser from "body parser";
+import session from "express-session"
+//import path from "path";
+import crypto from "crypto";*/
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 
+
+app.use('/includes', express.static(staticPath));
+app.use('/css', express.static(cssPath));
+
 app.set('views', path.join(__dirname, 'pages'));
 
 app.set('view engine', 'ejs');
-
-app.use('/includes', express.static(path.join(__dirname, 'includes')));
 
 app.get('/', (req, res) => {
   res.render('all/Login');
@@ -16,6 +26,7 @@ app.get('/', (req, res) => {
 app.get('/signup', (req, res) => {
   res.render('asdos/sign-up');
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);

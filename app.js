a43598cfaa7f3decfdc81e6f20cbe02988f5c8b9
Devlin <devlin.venedict@gmail.com>
2023-11-30 10:53:07 +0700

    perbaiki js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'pages'));
+
 app.set('view engine', 'ejs');
 
-app.set('views', app.js + '/views');
+app.use('/includes', express.static(path.join(__dirname, 'includes')));
 
 app.get('/', (req, res) => {
-  res.render('login');
+  res.render('all/Login');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('asdos/sign-up');
 });
 
 app.listen(port, () => {
